8923c2e5af30250b6c8ac274221985bb42e987bd
Nemanja Mijailovic <metalnem@mijailovic.net>
2025-06-06 22:17:28 +0200

    Add support for algorithm 1

--- Api/Headers.cs
+++ Api/Headers.cs
@@ -6,69 +6,69 @@ namespace Api;
 
 public static class Headers
 {
-    private const int AlgorithmHmacSha256 = 3;
-    private const int AlgorithmHmacSha512 = 4;
+	private const int AlgorithmHmacSha256 = 3;
+	private const int AlgorithmHmacSha512 = 4;
 
-    public static string CalculateDm(string dm, string requestBody)
-    {
-        int algorithm = 0;
+	public static string CalculateDm(string dm, string requestBody)
+	{
+		int algorithm = 0;
 
-        if (dm != null)
-        {
-            int index = dm.LastCharToInt();
-            algorithm = dm.CharToInt(index);
-        }
+		if (dm != null)
+		{
+			int index = dm.LastCharToInt();
+			algorithm = dm.CharToInt(index);
+		}
 
-        if (algorithm == AlgorithmHmacSha256)
-        {
-            var key = Sha256KdfV1.DeriveKey(dm);
-            var prefix = Sha256KdfV2.DeriveKey(dm);
+		if (algorithm == AlgorithmHmacSha256)
+		{
+			var key = Sha256KdfV1.DeriveKey(dm);
+			var prefix = Sha256KdfV2.DeriveKey(dm);
 
-            return Crypto.HmacSha256(key, prefix + requestBody);
-        }
+			return Crypto.HmacSha256(key, prefix + requestBody);
+		}
 
-        if (algorithm == AlgorithmHmacSha512)
-        {
-            throw new Exception("HMAC SHA-516 KDF is currently not supported.");
-        }
+		if (algorithm == AlgorithmHmacSha512)
+		{
+			throw new Exception("HMAC SHA-516 KDF is currently not supported.");
+		}
 
-        return Crypto.HmacSha1("dtmfa", Sha1Kdf.DeriveKey(dm) + requestBody);
-    }
+		return Crypto.HmacSha1("dtmfa", Sha1Kdf.DeriveKey(algorithm, dm) + requestBody);
+	}
 
-    public static string CalculatePow(string pow, string requestBody)
-    {
-        var parts = pow.Split(":");
-        var data = requestBody + parts[2];
-        var leadingZeroBits = int.Parse(parts[1]);
-        var nonce = CalculateNonce(data, leadingZeroBits);
+	public static string CalculatePow(string pow, string requestBody)
+	{
+		var parts = pow.Split(":");
+		var data = requestBody + parts[2];
+		var leadingZeroBits = int.Parse(parts[1]);
+		var nonce = CalculateNonce(data, leadingZeroBits);
 
-        return $"{parts[0]}:{parts[1]}:{parts[2]}:{nonce}";
-    }
+		return $"{parts[0]}:{parts[1]}:{parts[2]}:{nonce}";
+	}
 
-    private static string CalculateNonce(string data, int leadingZeroBits)
-    {
-        for (int nonce = 0; ; ++nonce)
-        {
-            var result = Utils.ToBase36(nonce);
-            var bytes = Encoding.UTF8.GetBytes(data + result);
-            var digest = SHA1.HashData(bytes);
-            var total = 0;
+	private static string CalculateNonce(string data, int leadingZeroBits)
+	{
+		for (int nonce = 0; ; ++nonce)
+		{
+			var result = Utils.ToBase36(nonce);
+			var bytes = Encoding.UTF8.GetBytes(data + result);
+			var digest = SHA1.HashData(bytes);
+			var total = 0;
 
-            for (int i = 0; i < digest.Length; ++i)
-            {
-                int count = BitOperations.LeadingZeroCount(digest[i]) - 24;
-                total += count;
+			for (int i = 0; i < digest.Length; ++i)
+			{
+				int count = BitOperations.LeadingZeroCount(digest[i]) - 24;
+				total += count;
 
-                if (count < 8)
-                {
-                    break;
-                }
-            }
+				if (count < 8)
+				{
+					break;
+				}
+			}
 
-            if (total >= leadingZeroBits)
-            {
-                return result;
-            }
-        }
-    }
+			if (total >= leadingZeroBits)
+			{
+				return result;
+			}
+		}
+	}
 }
--- Api/Sha1Kdf.cs
+++ Api/Sha1Kdf.cs
@@ -2,21 +2,26 @@ namespace Api;
 
 public static class Sha1Kdf
 {
-    public static string DeriveKey(string dm)
-    {
-        if (dm == null)
-        {
-            return string.Empty;
-        }
+	public static string DeriveKey(int algorithm, string dm)
+	{
+		if (dm == null)
+		{
+			return string.Empty;
+		}
 
-        string key = dm.ToUpper();
-        int floor = (int)Math.Floor(key.Length / 2.0);
+		if (algorithm == 1)
+		{
+			return dm.Substring(0, 19) + dm.Substring(22);
+		}
 
-        key = key.Substring(0, floor).Reverse() + key.Substring(floor).Reverse();
-        key = key.Substring(0, 15) + key.Substring(19);
-        key = key.Replace("4", "1");
-        key = Crypto.HmacSha1("bntpd", key);
+		string key = dm.ToUpper();
+		int floor = (int)Math.Floor(key.Length / 2.0);
 
-        return key.Replace("3", "5");
-    }
+		key = key.Substring(0, floor).Reverse() + key.Substring(floor).Reverse();
+		key = key.Substring(0, 15) + key.Substring(19);
+		key = key.Replace("4", "1");
+		key = Crypto.HmacSha1("bntpd", key);
+
+		return key.Replace("3", "5");
+	}
 }
